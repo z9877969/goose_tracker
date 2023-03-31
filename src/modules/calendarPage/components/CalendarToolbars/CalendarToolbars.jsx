e56a09392f534068409d4e82ab92948239654fba
z9877969 <z9877969@ukr.net>
2023-03-31 03:22:01 +0300

    finishes toolbars without operations

--- src/modules/calendarPage/components/CalendarToolbars/CalendarToolbars.jsx
+++ src/modules/calendarPage/components/CalendarToolbars/CalendarToolbars.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import moment from "moment";
 import { useCalendarPeriod } from "shared/hooks/useCalendarPeriod";
-import { routes } from "shared/services/routes";
 import { PeriodPaginator } from "../PeriodPaginator/PeriodPaginator";
 import { PeriodTypeSelect } from "../PeriodTypeSelect/PeriodTypeSelect";
 import s from "./CalendarToolbars.module.scss";
@@ -10,24 +9,6 @@ export const CalendarToolbars = () => {
   const periodType = useCalendarPeriod();
 
   const [date, setDate] = useState(() => moment().format("YYYY-MM-DD"));
-
-  const getCurPeriod = () => {
-    switch (periodType) {
-      case periodType === routes.CALENDAR_MONTH:
-        console.log(periodType);
-        break;
-      case periodType === routes.CALENDAR_WEEK:
-        console.log(periodType);
-        break;
-      case periodType === routes.CALENDAR_DAY:
-        console.log(periodType);
-        break;
-      default:
-        console.log(periodType);
-    }
-  };
-
-  getCurPeriod();
 
   return (
     <div className={s.wrapper}>
